679511b0c03ed6a88adc15766d9e9795bdc4e89e
Benjamin Arteaga <bnjmn_rtg@yahoo.com>
2016-10-20 00:26:35 -0500

    phone numbers

--- js/script.js
+++ js/script.js
@@ -1,15 +1,89 @@
 // =============== Phone Number Appear on Click =========================
-$('#arturo').on('click', function(){
-	$('#arturo').html("Hmn. Arturo Soto (512)-555-5555");
+// var leaders = [
+// 	{
+// 		'id':'arturo',
+// 		'name': 'Arturo Soto',
+// 		'phoneNumber': '(512)-555-1111'
+// 	},
+// 	{
+// 		'id':'alejandro',
+// 		'name': 'Alejandro Martinez',
+// 		'phoneNumber': '(512)-555-2222'
+// 	},
+// 	{
+// 		'id':'juan',
+// 		'name': 'Juan Marquez',
+// 		'phoneNumber': '(512)-555-3333'
+// 	},
+// 	{
+// 		'id':'israel',
+// 		'name': 'Israel Jaimes',
+// 		'phoneNumber': '(512)-555-4444'
+// 	}
+// ]
+
+// for (var i = 0; i < leaders.length; i++) {
+// 	$('#'+leaders[i].id).click(function(){
+// 		console.log("clicking works");
+// 		console.log(leaders[i].id);
+// 		$("#"+leaders[i].id).html("Hmn." + leaders[i].name + " " + leaders[i].phoneNumber);
+// 		var clicks = $(this).data('clicks');
+// 		if (clicks) {
+// 			$(this).html("Hmn." + leaders[i].name);
+// 		}
+// 		else{
+// 			$(this).html("Hmn." + leaders[i].name + " " + leaders[i].phoneNumber);
+
+// 		}
+// 		$(this).data("clicks", !clicks);
+
+// 	});
+// };
+// ===========Working Product======================
+$('#arturo').click(function(){
+	$('#arturo').html("Hmn. Arturo Soto (512)-555-1111");
+	var clicks = $(this).data('clicks');
+	if (clicks) {
+		$('#arturo').html("Hmn. Arturo Soto");
+	}
+	else{
+		$('#arturo').html("Hmn. Arturo Soto (512)-555-5555");
+
+	}
+	$(this).data("clicks", !clicks);
 });
-$('#alejandro').on('click', function(){
+$('#alejandro').click(function(){
 	$('#alejandro').html("Hmn. Alejandro Martinez (512)-555-5555");
+	var clicks = $(this).data('clicks');
+	if (clicks) {
+		$('#alejandro').html("Hmn. Alejandro Martinez");
+	}
+	else{
+		$('#alejandro').html("Hmn. Alejandro Martinez (512)-555-5555");
+	}
+	$(this).data("clicks", !clicks);
 });
-$('#juan').on('click', function(){
+$('#juan').click(function(){
 	$('#juan').html("Hmn. Juan Marquez (512)-555-5555");
+	var clicks = $(this).data('clicks');
+	if (clicks) {
+		$('#juan').html("Hmn. Juan Marquez");
+	}
+	else{
+		$('#juan').html("Hmn. Juan Marquez (512)-555-5555");
+	}
+	$(this).data("clicks", !clicks);
 });
-$('#israel').on('click', function(){
+$('#israel').click(function(){
 	$('#israel').html("Hmn. Israel Jaimes (512)-555-5555");
+	var clicks = $(this).data('clicks');
+	if (clicks) {
+		$('#israel').html("Hmn. Israel Jaimes");
+	}
+	else{
+		$('#israel').html("Hmn. Israel Jaimes (512)-555-5555");
+	}
+	$(this).data("clicks", !clicks);
 });
 // =======================================================================
 
@@ -27,4 +101,4 @@ $('#israel').on('click', function(){
 // 		console.log(i);
 // 	}
 // };
-// =======================================================================
+// ======================================================================= 
