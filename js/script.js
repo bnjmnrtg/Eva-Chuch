4c1747849c4a238ac0f2ed63cf92bb42f0c02dd0
Benjamin Arteaga <bnjmn_rtg@yahoo.com>
2016-10-19 22:50:56 -0500

    phone numbers

--- js/script.js
+++ js/script.js
@@ -1,11 +1,30 @@
-for (var i = 0; i < 100; i++){
-	if (i % 3 === 0) {
-		console.log(i +" =fizz");
-	}
-	if (i % 5 === 0) {
-		console.log(i +" =buzz");
-	}
-	else{
-		console.log(i);
-	}
-};
+// =============== Phone Number Appear on Click =========================
+$('#arturo').on('click', function(){
+	$('#arturo').html("Hmn. Arturo Soto (512)-555-5555");
+});
+$('#alejandro').on('click', function(){
+	$('#alejandro').html("Hmn. Alejandro Martinez (512)-555-5555");
+});
+$('#juan').on('click', function(){
+	$('#juan').html("Hmn. Juan Marquez (512)-555-5555");
+});
+$('#israel').on('click', function(){
+	$('#israel').html("Hmn. Israel Jaimes (512)-555-5555");
+});
+// =======================================================================
+
+
+
+// ============================ Fizz Buzz ================================
+// for (var i = 0; i < 100; i++){
+// 	if (i % 3 === 0) {
+// 		console.log(i +" =fizz");
+// 	}
+// 	if (i % 5 === 0) {
+// 		console.log(i +" =buzz");
+// 	}
+// 	else{
+// 		console.log(i);
+// 	}
+// };
+// =======================================================================
